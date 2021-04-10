976c7313c7e7a411de661103a96baa4b8aba7339
El-Gamily <mahmoud.elgamily@everestminds.com>
2021-04-10 04:12:21 +0200

    refactoring

--- April2021/9- VerifyinganAlienDictionary.js
+++ April2021/9- VerifyinganAlienDictionary.js
@@ -5,28 +5,23 @@
  */
 
 const isSorted = (arr) => {
-  let result = true;
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[i - 1]) {
-      result = false;
-      break;
-    }
-  }
-  return result;
+  let i = 1;
+  for (; i < arr.length; i++) if (arr[i] < arr[i - 1]) break;
+  return i == arr.length;
 };
 const isAlienSorted = function (words, order) {
   if (words.length == 1) return true;
 
-  const weight = {};
-  order.split('').map((letter, index) => (weight[letter] = index + 1));
+  const weight = order.split('').reduce((acc, letter, index) => {
+    return { ...acc, [letter]: index + 1 };
+  }, {});
 
-  const maxLength = Math.max(...words.map((el) => el.length));
+  const maxLength = Math.max(...words.map((word) => word.length));
 
   const wordsWeight = words.map((word) =>
     word.split('').reduce((acc, letter, index) => {
       return acc + 10 ** (maxLength - 1 - index) * weight[letter];
     }, 0)
   );
-  console.log(wordsWeight);
   return isSorted(wordsWeight);
 };
