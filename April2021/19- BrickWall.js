8e18b05ac4c9c1743df3a090504e743ae61125bf
El-Gamily <mahmoud.elgamily@everestminds.com>
2021-04-22 22:06:58 +0200

    day 19 optimized

--- April2021/19- BrickWall.js
+++ April2021/19- BrickWall.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var leastBricks = function (wall) {
-  const gaps = {};
+  const gaps = { max: 0 };
   for (let row of wall) {
     if (row.length == 1) {
       gaps[row[0]] = 0;
@@ -12,8 +12,9 @@ var leastBricks = function (wall) {
     let sum = row[0];
     for (let i = 1; i < row.length; i++) {
       gaps[sum] ? gaps[sum]++ : (gaps[sum] = 1);
+      if (gaps.max < gaps[sum]) gaps.max = gaps[sum];
       sum += row[i];
     }
   }
-  return wall.length - Math.max(...Object.values(gaps));
+  return wall.length - gaps.max;
 };
