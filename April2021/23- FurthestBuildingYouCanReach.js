4fedf80911ecded8df23819459456d176599404c
El-Gamily <mahmoud.elgamily@everestminds.com>
2021-05-01 04:15:10 +0200

    day 29

--- April2021/23- FurthestBuildingYouCanReach.js
+++ April2021/23- FurthestBuildingYouCanReach.js
@@ -6,11 +6,19 @@
  */
 var furthestBuilding = function (heights, bricks, ladders) {
   let queue = [];
+  queue.add = (diff) => {
+    for (let i = 0; i < queue.length; i++) {
+      if (queue[i] > diff) {
+        queue = [...queue.slice(0, i), diff, ...queue.slice(i)];
+        break;
+      }
+    }
+  };
   for (let i = 0; i < heights.length; i++) {
     let diff = heights[i + 1] - heights[i];
     if (diff > 0) {
       if (diff > 0 && ladders) {
-        queue.push(diff);
+        queue.add(diff);
         ladders--;
       } else if (!ladders && (bricks - queue[0] >= 0 || bricks > diff)) {
         if (diff > queue[0]) {
